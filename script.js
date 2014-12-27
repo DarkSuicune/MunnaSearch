990444429fcf0ff234057868cf0193c66735dc23
Dubstepper <Dubstepper@users.noreply.github.com>
2014-12-27 19:03:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 $(function(){
     
+    $('.Pokemon').keypress(function(event){
+        if(event.which == 13){
+            $('.catbutton').click();
+        }
+    });
+    
     $('.catbutton').click(function(){
         var poke = $('.Pokemon').val();
         $('.result').html('<span class="result">' + poke + ', huh? We\'ve got some pretty cool stuff about ' + poke + '!</span>');
