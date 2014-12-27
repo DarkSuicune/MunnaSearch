45632b0666b40d13b28b6c5bae8427b6acaf6435
Dubstepper <Dubstepper@users.noreply.github.com>
2014-12-27 18:41:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 $(function(){
+    var pokemon = [
+        'Munna': {
+        location: 'That dreamyard place',
+        recommendedItem: 'Choice Specs'
+    }, 
+    'Pikachu': {
+        location: 'Electric places',
+        otherInfo: 'hey'
+    }
+    ];
     $('.catbutton').click(function(){
-    $('.result').html('<span class="result">Munnas everywhere :D</span>');
+        var pokeToFind = $('.result').val();
+        for(x = 0; x < pokemon.length; x++){
+            if(pokemon[pokeToFin]){
+                $('.result').html('<span class="result">' + pokemon[pokeToFind] + '</span>');
+            } else {
+                $('.result').html('<span class="result">We couldn\'t find that Poke D;</span>');
+            }
+        }
   });
 });
