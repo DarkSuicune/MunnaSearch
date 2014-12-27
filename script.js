e3c16ec67ba13ee410ba71ecbef39f197d052808
Dubstepper <Dubstepper@users.noreply.github.com>
2014-12-27 18:58:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(function(){
     
     $('.catbutton').click(function(){
-        var poke = $('.result').val();
+        var poke = $('.Pokemon').val();
         $('.result').html('<span class="result">' + poke + ', huh? We\'ve got some pretty cool stuff about Munna!</span>');
   });
 });
