83897434774bd5954bc5b8e07247daffbf1f6286
DarkSuicune <ckasslertaub@pclcnj.com>
2014-12-28 18:31:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,6 @@ $(function(){
     
     $('.catbutton').click(function(){
         var poke = $('.Pokemon').val();
-        $('.result').html('<span class="result">' + poke + ', huh? We\'ve got some pretty cool stuff about ' + poke + '!</span>');
+        $('.result').html('<span class="result">' How to breed the perfect ' + poke + '!</span>');
   });
 });
