b39f812a3f916f0b1c756dc7339aec042e56aa69
DarkSuicune <ckasslertaub@pclcnj.com>
2014-12-29 12:06:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -133,7 +133,7 @@ $(function(){
         '<p class="moveinfo">Gender: 50% male - 50% female</p>' +
         '<p class="moveinfo">' + $('.item :selected').text() +': Found at ' + items[$('.item :selected').text()] + '</p>' +
         '<p class="moveinfo">EVs: Time to EV train :D!</p>' +
-        '<p class="moveinfo">' + $('.nature :selected').text() + ' Nature: Breed with a $(' .nature :selected')' Ditto holding an Everstone</p>' +
+        '<p class="moveinfo">' + $('.nature :selected').text() + ' Nature: Breed with a (' .nature :selected')' Ditto holding an Everstone</p>' +
         '<p class="moveinfo">' + $('.move-1 :selected').text() + ': Hey</p>' +
         '<p class="moveinfo">' + $('.move-2 :selected').text() + ': Hey</p>' +
         '<p class="moveinfo">' + $('.move-3 :selected').text() + ': Hey</p>' +
