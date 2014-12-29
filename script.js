e26521c0af53f1e3840f9e33aae3ead542f1c3de
Dubstepper <Dubstepper@users.noreply.github.com>
2014-12-29 01:24:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ $(function(){
     
     $('.go').click(function(){
         var poke = $('.Pokemon').val();
-        $('.result-container').html('<span class="result">How to breed the perfect ' + poke + '!</span>');
+        $('.result-container').html('<span class="result">How to breed the perfect ' + poke + '!</span>' +
+        'Gender: 50% male - 50% female' +
+        'Item: Found in Granite Cave, Hoenn' +
+        'EVs: Time to EV train :D!' +
+        'Nature: Breed with a timid Ditto holding an Everstone' +
+        'Move 1: Hey' +
+        'Move 2: Hey' +
+        'Move 3: Hey' +
+        'Move 4: Hey'
+        );
   });
 });
