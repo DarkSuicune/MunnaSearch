9984e5934d2df86911468bf97f6f01dcbab928d2
Dubstepper <Dubstepper@users.noreply.github.com>
2014-12-29 22:23:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -330,7 +330,7 @@ $(function(){
         
         $('.result-container').html('<span class="result">How to breed the perfect ' + poke + '!</span>' +
         '<p class="moveinfo">' + poke + ': Found at ' + locations[poke] + '</p>' +
-        '<p class="moveinfo">' + $('.item :selected').text() +': Found at ' + items[$('.item :selected').text()] + '</p>' +
+        '<p class="moveinfo">' + $('.item :selected').text() +': ' + items[$('.item :selected').text()] + '</p>' +
         '<p class="moveinfo">EVs: ' + hp + attack + defense + spattack + spdefense + speed + '</p>' +
         '<p class="moveinfo">' + $('.nature :selected').text() + ' Nature: Breed with a ' + $('.nature :selected').text() + ' nature Ditto holding an Everstone </p>' +
         '<p class="moveinfo">' + $('.move-1 :selected').text() + ': ' + moves[$('.move-1 :selected').text()] + '</p>' +
