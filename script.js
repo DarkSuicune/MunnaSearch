e22151f1ae7b9e9170f774bdeae05a38f116ff5c
Dubstepper <Dubstepper@users.noreply.github.com>
2014-12-29 01:35:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ $(function(){
     $('.go').click(function(){
         var poke = $('.Pokemon').val();
         $('.result-container').html('<span class="result">How to breed the perfect ' + poke + '!</span>' +
-        'Gender: 50% male - 50% female' +
-        'Item: Found in Granite Cave, Hoenn' +
-        'EVs: Time to EV train :D!' +
-        'Nature: Breed with a timid Ditto holding an Everstone' +
+        '<p class="moveinfo">Gender: 50% male - 50% female</p>' +
+        '<p class="moveinfo">Item: Found in Granite Cave, Hoenn</p>' +
+        '<p class="moveinfo">EVs: Time to EV train :D!</p>' +
+        '<p class="moveinfo">Nature: Breed with a timid Ditto holding an Everstone</p>' +
         '<p class="moveinfo">Move 1: Hey</p>' +
         '<p class="moveinfo">Move 2: Hey</p>' +
         '<p class="moveinfo">Move 3: Hey</p>' +
