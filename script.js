f0072342e1c66bce5812499372521c864a39b823
Dubstepper <Dubstepper@users.noreply.github.com>
2014-12-29 02:22:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ $(function(){
         var poke = $('.Pokemon').val();
         $('.result-container').html('<span class="result">How to breed the perfect ' + poke + '!</span>' +
         '<p class="moveinfo">Gender: 50% male - 50% female</p>' +
-        '<p class="moveinfo">' + $('.nature :selected').text() +': Found at ' + items[$('.nature :selected').text()] + '</p>' +
+        '<p class="moveinfo">' + $('.item :selected').text() +': Found at ' + items[$('.item :selected').text()] + '</p>' +
         '<p class="moveinfo">EVs: Time to EV train :D!</p>' +
         '<p class="moveinfo">' + $('.nature :selected').text() + ' Nature: Breed with a timid Ditto holding an Everstone</p>' +
         '<p class="moveinfo">' + $('.move-1 :selected').text() + ': Hey</p>' +
